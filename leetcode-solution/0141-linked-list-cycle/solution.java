239fb649ad9e5de95d80a6d44496ffac671efcde
Ankur Budhauliya <ankurbudhauliya@gmail.com>
2025-11-09 21:38:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.9 MB (9.71%)

--- leetcode-solution/0141-linked-list-cycle/solution.java
+++ leetcode-solution/0141-linked-list-cycle/solution.java
@@ -11,13 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set=new HashSet<>();
-        while (head!=null) {
-            if (set.contains(head)) {
-                return true;  
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
             }
-            set.add(head);
-            head = head.next;
+            
         }
         return false;
     }
