47364ba4b305a0eb6945d4476617dd1d6c6181ea
Ankur Budhauliya <ankurbudhauliya@gmail.com>
2025-11-16 17:26:08 +0000

    [LeetCode Sync] Runtime - 5 ms (66.29%), Memory - 45.9 MB (10.10%)

---
+++ leetcode-solution/4112-maximize-expression-of-three-elements/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        int result = (nums[n-1]+nums[n-2]-nums[0]);
+        
+        return result;
+    }
+
+}
