e1d56628b0e6cb601c3b9875218e71e4a7bf46fb
Ankur Budhauliya <ankurbudhauliya@gmail.com>
2025-11-19 06:41:26 +0000

    [LeetCode Sync] Runtime - 130 ms (3.70%), Memory - 60.3 MB (5.03%)

--- leetcode-solution/0344-reverse-string/solution.java
+++ leetcode-solution/0344-reverse-string/solution.java
@@ -1,13 +1,19 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length-1;
-        while(left<=right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        char[] reversed = reverse(s, 0);
+
+        for (int i = 0; i < s.length; i++) {
+            s[i] = reversed[i];
         }
+    }
+    public char[] reverse(char[] s, int index){
+        if(index == s.length){
+            return new char[0];
+        }
+        char[] rest = reverse(s, index+1);
+
+        char[] result = Arrays.copyOf(rest,rest.length+1);
+        result[rest.length] = s[index];
+        return result;
     }
 }
