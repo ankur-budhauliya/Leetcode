a4147253461d49fdabde771d12b6697ce793b064
Ankur Budhauliya <ankurbudhauliya@gmail.com>
2025-11-20 04:29:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.5 MB (9.14%)

--- leetcode-solution/0792-binary-search/solution.java
+++ leetcode-solution/0792-binary-search/solution.java
@@ -1,19 +1,16 @@
 class Solution {
     public int search(int[] nums, int target) {
-       int start = 0;
-       int end = nums.length-1;
-       while(start <= end){
-        int mid = start + (end - start)/2;
-        if(nums[mid] == target){
-            return mid;
+        return helper(nums,target,0,nums.length-1);
+        
+    }
+    public int helper(int[] nums,int target, int l, int r){
+        if(l>r) return -1;
+        int mid = l+(r-l)/2;
+        if(nums[mid] == target) return mid;
+        if(nums[mid] > target){
+            return helper(nums,target,l,mid-1);
+        }else{
+            return helper(nums,target,mid+1,r);
         }
-        else if(nums[mid] > target){
-            end = mid-1;
-        }
-        else{
-            start = mid+1;
-        }
-       }
-       return -1; 
     }
 }
