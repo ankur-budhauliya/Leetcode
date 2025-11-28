5d412fdf2fcd89a5b0eb2d2497de55c38918be30
Ankur Budhauliya <ankurbudhauliya@gmail.com>
2025-11-28 06:19:08 +0000

    [LeetCode Sync] Runtime - 1 ms (21.28%), Memory - 43.4 MB (22.32%)

--- leetcode-solution/0075-sort-colors/solution.java
+++ leetcode-solution/0075-sort-colors/solution.java
@@ -1,25 +1,13 @@
 class Solution {
     public void sortColors(int[] nums) {
-
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                count0++;
-                
-            } 
-            else if( nums[i] == 1){
-                count1++;
-            } 
-            else{
-                count2++;
+       for(int i = 0; i< nums.length; i++){
+        for(int j = i+1; j < nums.length; j++){
+            if(nums[i] > nums[j]){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
             }
         }
-        int index = 0;
-        for(int i =0; i< count0; i++) nums[index++] = 0;
-        for(int i =0; i< count1; i++) nums[index++] = 1;
-        for(int i =0; i< count2; i++) nums[index++] = 2;
+       } 
     }
 }
