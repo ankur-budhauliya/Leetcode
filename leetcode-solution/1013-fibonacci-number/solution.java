50a3856c2be5a9ae66fe6b7a9d933c15017665c7
Ankur Budhauliya <ankurbudhauliya@gmail.com>
2025-11-30 08:46:50 +0000

    [LeetCode Sync] Runtime - 10 ms (17.15%), Memory - 42.3 MB (5.26%)

--- leetcode-solution/1013-fibonacci-number/solution.java
+++ leetcode-solution/1013-fibonacci-number/solution.java
@@ -1,8 +1,10 @@
 class Solution {
     public int fib(int n) {
-        if(n<2){
-            return n;
-        }
-        return fib(n-1) + fib(n-2);
+        return fibo(n);
+    }
+    public int fibo(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fibo(n-2) + fibo(n-1);
     }
 }
