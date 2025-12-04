56c151438c01e4777f5d3262d96831985d781acd
Ankur Budhauliya <ankurbudhauliya@gmail.com>
2025-12-04 04:40:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (23.74%)

--- leetcode-solution/1013-fibonacci-number/solution.java
+++ leetcode-solution/1013-fibonacci-number/solution.java
@@ -1,10 +1,28 @@
 class Solution {
     public int fib(int n) {
-        return fibo(n);
+        int[] dp = new int[n+1];
+        if(n <= 1) return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i < n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
-    public int fibo(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fibo(n-2) + fibo(n-1);
-    }
+
+
+    // Memoization
+    // public int fib(int n) {
+    //     int[] dp = new int[n + 1];
+    //     return fibonacci(n, dp);
+    // }
+    
+    // public int fibonacci(int n, int[] dp) {
+    //     if (n <= 1)
+    //         return n;
+    //     if (dp[n] != 0)
+    //         return dp[n];
+    //     return dp[n] = fibonacci(n - 1, dp) + fibonacci(n - 2, dp);
+
+    // }
 }
